cba3b2f60f7fa02513fdba0e09dd2316eec466e8
Vivek9i080 <138898074+Vivek9i080@users.noreply.github.com>
2024-07-16 23:23:04 +0530

    Add files via upload

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,18 +1,17 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import {getAuth} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
+import {getStorage} from "firebase/storage";
 
 const firebaseConfig = {
-	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-	appId: import.meta.env.VITE_FIREBASE_APP_ID,
-	measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-};
-
+  apiKey: "AIzaSyBj6Eh2REJkho0DhNtxXN8wH2J0gZ0hdUY",
+  authDomain: "insta-clone-4186f.firebaseapp.com",
+  projectId: "insta-clone-4186f",
+  storageBucket: "insta-clone-4186f.appspot.com",
+  messagingSenderId: "448969418812",
+  appId: "1:448969418812:web:8b2da834ea8308d1573b39",
+  measurementId: "G-E7YZZ51R81"
+}
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const firestore = getFirestore(app);
